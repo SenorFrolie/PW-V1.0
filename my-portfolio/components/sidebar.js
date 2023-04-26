8d2768a77dbe4768fe85f2eecec73d9562aa4740
Robert Bonham <reb159@txstate.edu>
2023-04-26 01:16:58 -0500

    added hover affects to the sidebar

--- my-portfolio/components/sidebar.js
+++ my-portfolio/components/sidebar.js
@@ -1,23 +1,25 @@
 import Link from 'next/link';
+import Image from 'next/image'
 
+import { avatar } from 'lib/info';
 
 
 export default function Navbar(){
     return (
-    <aside className='md:w-[100px] md:flex-shrink-0 -mx-4 md:mx-0 md:px-0 font-serif'>
+    <aside className='md:w-[110px] md:flex-shrink-0 -mx-4 md:mx-0 md:px-0 font-serif'>
     <div className='lg:sticky lg:top-20'>
         <nav
         className="flex flex-row md:flex-col items-start relative px-4 md:px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
         id="nav"
         >
           <div className="flex flex-row md:flex-col space-x-0 pr-10 mb-2 mt-2 md:mt-0">
-            <div className='dark:hover:text-neutral-200 flex align-middle'>
-            <ul>
-                <li><Link href='/'>Home</Link></li>
-                <li><Link href='/about'>About</Link></li>
-                <li><Link href='/projects'>Projects</Link></li>
-                <li><Link href='/blog'>Blog</Link></li>
-            </ul>
+            <div className='flex align-middle text-[16.5px] text-base/[35px]'>
+              <ul>
+                  <li><Link href='/' className='px-1 py-1 text-white-700 rounded-lg hover:bg-slate-100 hover:text-slate-900'>Home</Link></li>
+                  <li><Link href='/about' className='px-1 py-1 text-white-700 rounded-lg hover:bg-slate-100 hover:text-slate-900'>About</Link></li>
+                  <li><Link href='/projects' className='px-1 py-1 text-white-700 rounded-lg hover:bg-slate-100 hover:text-slate-900'>Projects</Link></li>
+                  <li><Link href='/blog' className='px-1 py-1 text-white-700 rounded-lg hover:bg-slate-100 hover:text-slate-900'>Blog</Link></li>
+              </ul>
             </div>
           </div>
         </nav>
