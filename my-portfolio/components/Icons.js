3dbf22230b4178f287487b59d701ea64679b231b
Robert Bonham <rbonham21@gmail.com>
2023-05-23 00:33:10 -0500

    deleted functions for Icons

--- my-portfolio/components/Icons.js
+++ my-portfolio/components/Icons.js
@@ -1,27 +1,3 @@
-export function TwitterIcon() {
-  return (
-    <svg
-      width="20"
-      height="16"
-      viewBox="0 0 24 20"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g clipPath="url(#a)">
-        <path
-          d="M21.479 4.937c.015.209.015.418.015.628 0 6.424-4.917 13.832-13.906 13.832v-.004a13.89 13.89 0 0 1-7.491-2.18 9.847 9.847 0 0 0 7.233-2.015 4.89 4.89 0 0 1-4.566-3.375c.732.14 1.487.112 2.206-.084a4.868 4.868 0 0 1-3.92-4.764v-.062c.68.376 1.44.585 2.218.608a4.851 4.851 0 0 1-1.513-6.49 13.896 13.896 0 0 0 10.073 5.078 4.848 4.848 0 0 1 1.414-4.644 4.911 4.911 0 0 1 6.914.21A9.84 9.84 0 0 0 23.26.496a4.884 4.884 0 0 1-2.149 2.69 9.76 9.76 0 0 0 2.807-.766 9.898 9.898 0 0 1-2.439 2.518Z"
-          fill="currentColor"
-        />
-      </g>
-      <defs>
-        <clipPath id="a">
-          <path fill="#fff" d="M0 0h24v19.636H0z" />
-        </clipPath>
-      </defs>
-    </svg>
-  );
-}
-
 export function GitHubIcon() {
   return (
     <svg
@@ -46,26 +22,6 @@ export function GitHubIcon() {
   );
 }
 
-export function ViewsIcon() {
-  return (
-    <svg
-      className="mr-2"
-      width="20"
-      height="20"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M2.25 18.0001L9 11.2501L13.306 15.5571C14.5507 13.1029 16.6044 11.1535 19.12 10.0381L21.86 8.81809M21.86 8.81809L15.92 6.53809M21.86 8.81809L19.58 14.7591"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-}
 
 export function ArrowIcon() {
   return (
@@ -81,21 +37,6 @@ export function ArrowIcon() {
         d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
         fill="currentColor"
       />
-    </svg>
-  );
-}
-
-export function YoutubeIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      height="800"
-      width="800"
-      viewBox="0 0 461.001 461.001"
-      className="w-5 h-5"
-      fill="currentColor"
-    >
-      <path d="M365.257 67.393H95.744C42.866 67.393 0 110.259 0 163.137v134.728c0 52.878 42.866 95.744 95.744 95.744h269.513c52.878 0 95.744-42.866 95.744-95.744V163.137c0-52.878-42.866-95.744-95.744-95.744zm-64.751 169.663-126.06 60.123c-3.359 1.602-7.239-.847-7.239-4.568V168.607c0-3.774 3.982-6.22 7.348-4.514l126.06 63.881c3.748 1.899 3.683 7.274-.109 9.082z" />
     </svg>
   );
 }
--- my-portfolio/src/app/about/page.js
+++ my-portfolio/src/app/about/page.js
@@ -1,8 +1,6 @@
 import {
     GitHubIcon,
-    YoutubeIcon,
     ArrowIcon,
-    TwitterIcon,
     LinkedinIcon
   } from 'components/icons';
   
